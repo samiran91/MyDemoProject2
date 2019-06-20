b3ae26b0e17ffffa3f747011cda5aba5c65b6bc5
samiran91 <samiransen.it@gmail.com>
2019-06-20 22:49:57 +0530

    Cookie code added.

--- Jobsportal/Jobsportal/Scripts/JobList/JobList.js
+++ Jobsportal/Jobsportal/Scripts/JobList/JobList.js
@@ -1,5 +1,7 @@
 ﻿
 $(document).ready(function () {
+    var value = document.cookie;
+    
     grid = $('#grid').grid({
         primaryKey: 'JOBNO',
         uiLibrary: "bootstrap",
@@ -7,10 +9,10 @@ $(document).ready(function () {
         
         dataSource: '/Jobs/GetJobList',
         columns: [
-            { field: 'JobNo', title: 'Job Number', width: 200, priority: 1 },
-            { field: 'JobTitle', title: 'Job Title', sortable: true, priority: 3 },
+            { field: 'JobNo', title: 'Job Number', width: 200, priority: 3 },
+            { field: 'JobTitle', title: 'Job Title', sortable: true, priority: 1 },
             { field: 'PostedDate', title: 'Posted Date', width: 200, sortable: true, type: 'date', priority: 2 },
-            //{ field: 'Qualification', title: 'Qualification', sortable: true },
+           
             {
                 field: "", title: "Apply Now", width: 100, type: "icon",
                 icon: "fa fa-sign-in fa-2X", tooltip: "Apply Now", events: { "click": ApplyForJob }
@@ -22,10 +24,21 @@ $(document).ready(function () {
 
     });
 });
+window.addEventListener("beforeunload", function (e) {
+   
 
+    var keyword = $("txt_keyword").text();
+    var posted = $("ddl_posted").text();
+    var location = $("ddl_location").text();
+    document.cookie = "keyword="+keyword;
+    document.cookie = "posted="+posted;
+    document.cookie = "location="+location;
+   
+    
+});
 
 function ApplyForJob(e) {
-
+   
     var JobNumber = e.data.record.JobNo;
     window.location.href = "/jobs/JobDetails?JNo=" + JobNumber;
 
