f23213eeff8e7c7819e5ffd38303352f91e14f7c
samiran91 <samiransen.it@gmail.com>
2019-06-20 21:09:17 +0530

    Some UX corrections

--- Jobsportal/Jobsportal/Scripts/JobList/JobList.js
+++ Jobsportal/Jobsportal/Scripts/JobList/JobList.js
@@ -2,20 +2,24 @@
 $(document).ready(function () {
     grid = $('#grid').grid({
         primaryKey: 'JOBNO',
+        uiLibrary: "bootstrap",
+        responsive: true,
         
         dataSource: '/Jobs/GetJobList',
         columns: [
-            { field: 'JobNo', title: 'Job Number', width: 200},
-            { field: 'JobTitle',title: 'Job Title', sortable: true },
-            { field: 'PostedDate', title: 'Posted Date', width: 200, sortable: true, type: 'date' },
+            { field: 'JobNo', title: 'Job Number', width: 200, priority: 1 },
+            { field: 'JobTitle', title: 'Job Title', sortable: true, priority: 3 },
+            { field: 'PostedDate', title: 'Posted Date', width: 200, sortable: true, type: 'date', priority: 2 },
             //{ field: 'Qualification', title: 'Qualification', sortable: true },
             {
                 field: "", title: "Apply Now", width: 100, type: "icon",
-                icon: "fa fa-pencil", tooltip: "Apply Now", events: { "click": ApplyForJob }
+                icon: "fa fa-sign-in fa-2X", tooltip: "Apply Now", events: { "click": ApplyForJob }
                 
             }
         ],
-        pager: { limit: 5 }
+        pager: { enable: true, limit: 5, sizes: [2, 5, 10, 20] }
+   
+
     });
 });
 
