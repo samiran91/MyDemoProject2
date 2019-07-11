77f3028fbac59a4a450d6160fd1dd365169bd21a
samiran91 <samiransen.it@gmail.com>
2019-07-11 11:42:40 +0530

    New files

---
+++ Jobsportal/Jobsportal/Controllers/CandidateController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DAL;
+using System.IO;
+
+namespace Jobsportal.Controllers
+{
+    
+    public class CandidateController : Controller
+    {
+        // GET: Candidate
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Authorize(Roles = "ADMIN,CANDIDATE")]
+        public ActionResult CandidateProfile()
+        {
+           DAL.Utility.LogActivity("At CandidateProfile", "CandidateProfile");
+            return View();
+        }
+        [Authorize(Roles = "ADMIN,CANDIDATE")]
+        public JsonResult FetchCandidateDetails()
+        {
+            string phoneno = System.Web.HttpContext.Current.User.Identity.Name;
+            Job.CandidateProfile CP = Job.FetchCandidateDetails(phoneno);
+            if (CP.ImgValue != null)
+            {
+                CP.ImgValue = "/Images/CandidateImages/" + CP.ImgValue;
+            }
+
+            return Json(CP, JsonRequestBehavior.AllowGet);
+        }
+        
+        public JsonResult SaveCandidateRecord(Job.CandidateProfile OBJ)
+        {
+            OBJ.Mobile = System.Web.HttpContext.Current.User.Identity.Name;
+            Job.CandidateProfile CP = Job.SaveCandidate(OBJ);
+
+            return Json(true);
+        }
+        [Authorize(Roles = "ADMIN,CANDIDATE")]
+        [HttpPost]
+        public JsonResult UploadFiles()
+        {
+            // Checking no of files injected in Request object  
+            if (Request.Files.Count > 0)
+            {
+                try
+                {
+                    //  Get all files from Request object
+                    string fname;
+                    HttpFileCollectionBase files = Request.Files;
+                    for (int i = 0; i < files.Count; i++)
+                    {
+                   
+
+                        HttpPostedFileBase file = files[i];
+
+
+                        // Checking for Internet Explorer  
+                        if (Request.Browser.Browser.ToUpper() == "IE" || Request.Browser.Browser.ToUpper() == "INTERNETEXPLORER")
+                        {
+                            string[] testfiles = file.FileName.Split(new char[] { '\\' });
+                            fname = testfiles[testfiles.Length - 1];
+                        }
+                        else
+                        {
+                            fname = file.FileName;
+                        }
+                        fname = Request.Files.AllKeys[0].ToString();
+                        // Get the complete folder path and store the file inside it.  
+                        fname = Path.Combine(Server.MapPath("~/Images/CandidateImages/"), fname);
+                        file.SaveAs(fname);
+                    }
+                    // Returns message that successfully uploaded  
+                    return Json("");
+                }
+                catch (Exception ex)
+                {
+                    return Json("File Upload Error occurred. Error details: " + ex.Message);
+                }
+            }
+            else
+            {
+                return Json("No files selected.");
+            }
+        }
+
+
+        
+    }
+}
---
+++ Jobsportal/Jobsportal/Scripts/utility.js
@@ -0,0 +1,44 @@
+﻿function formatDate(date) {
+    var datestr = parseJsonDate(date);
+
+    var d = new Date(datestr),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+
+    return [year, month, day].join('-');
+}
+function parseJsonDate(jsonDate) {
+
+    var fullDate = new Date(parseInt(jsonDate.substr(6)));
+    var twoDigitMonth = (fullDate.getMonth() + 1) + ""; if (twoDigitMonth.length == 1) twoDigitMonth = "0" + twoDigitMonth;
+
+    var twoDigitDate = fullDate.getDate() + ""; if (twoDigitDate.length == 1) twoDigitDate = "0" + twoDigitDate;
+    var currentDate = twoDigitMonth + "/" + twoDigitDate + "/" + fullDate.getFullYear();
+
+    return currentDate;
+};
+
+
+function GetParameterValues(param) {
+    var url = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for (var i = 0; i < url.length; i++) {
+        var urlparam = url[i].split('=');
+        if (urlparam[0] == param) {
+            return urlparam[1];
+        }
+    }
+}
+function isEmail(Email) {
+    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    return regex.test(Email);
+}
+
+
+function phonenumber(Mobile) {
+    var regex = /^\d{10}$/;
+    return regex.test(Mobile);
+}
